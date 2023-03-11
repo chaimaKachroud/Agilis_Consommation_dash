452de3020f14193f679e09095267157d7ac0ddb3
ChaimaKCH <chaimakachroud971@gmail.com>
2023-03-11 20:08:26 +0100

    commit the path of style ccs modification

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 # Import library
 import streamlit as st
 import pandas as pd
+import os 
 #from numerize.numerize import numerize
 #from PIL import Image
 #import streamlit.components.v1 as components
@@ -19,7 +20,7 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-with open('/Agilis_DashBoard/Agilis_Consommation_dash/style.css') as f:
+with open(os.path.join("./Agilis_DashBoard/Agilis_Consommation_dash/style.css")) as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 # put a global header for the home page
 st.header('متابعة التزود و إستهلاك الوقود بالبطاقة الذكية')
